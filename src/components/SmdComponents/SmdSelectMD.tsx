4a503e94f5dd2866bf60d0066a69b4be4b2c7c6b
andrey-omsk-63 <andrey_omsk@mail.ru>
2022-10-28 21:24:13 +0600

    '28192124'

--- src/components/SmdComponents/SmdSelectMD.tsx
+++ src/components/SmdComponents/SmdSelectMD.tsx
@@ -1,13 +1,16 @@
-import * as React from "react";
-import { useSelector } from "react-redux";
+import * as React from 'react';
+import { useSelector } from 'react-redux';
 
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
-import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
 
-import { styleModalEnd, styleSetInf } from "./../MainMapStyle";
+import { styleModalEnd } from './../MainMapStyle';
+
+let massName: any = [];
+let flagBegin = true;
 
 const SmdSelectMD = (props: { setOpen: any }) => {
   //== Piece of Redux =======================================
@@ -17,14 +20,14 @@ const SmdSelectMD = (props: { setOpen: any }) => {
   });
   //===========================================================
   const styleSetInf = {
-    position: "relative",
+    position: 'relative',
     marginTop: 4,
-    marginLeft: "auto",
+    marginLeft: 'auto',
     marginRight: 69,
     width: 420,
-    bgcolor: "background.paper",
-    border: "3px solid #000",
-    borderColor: "primary.main",
+    bgcolor: 'background.paper',
+    border: '3px solid #000',
+    borderColor: 'primary.main',
     borderRadius: 2,
     boxShadow: 24,
     p: 1.5,
@@ -33,40 +36,55 @@ const SmdSelectMD = (props: { setOpen: any }) => {
   const styleBut01 = {
     fontSize: 14,
     marginTop: 1,
-    border: "2px solid #000",
-    bgcolor: "#E6F5D6",
-    minWidth: "300px",
-    maxWidth: "300px",
-    maxHeight: "20px",
-    minHeight: "20px",
-    borderColor: "#E6F5D6",
+    border: '2px solid #000',
+    bgcolor: '#E6F5D6',
+    minWidth: '300px',
+    maxWidth: '300px',
+    maxHeight: '20px',
+    minHeight: '20px',
+    borderColor: '#E6F5D6',
     borderRadius: 2,
-    color: "black",
-    textTransform: "unset !important",
+    color: 'black',
+    textTransform: 'unset !important',
   };
 
   const styleBut02 = {
     fontSize: 12,
     marginTop: 1,
-    border: "2px solid #000",
-    bgcolor: "#E6F5D6",
-    minWidth: "12px",
-    maxWidth: "120px",
-    maxHeight: "20px",
-    minHeight: "20px",
-    borderColor: "#E6F5D6",
+    border: '2px solid #000',
+    bgcolor: '#E6F5D6',
+    minWidth: '12px',
+    maxWidth: '120px',
+    maxHeight: '20px',
+    minHeight: '20px',
+    borderColor: '#E6F5D6',
     borderRadius: 2,
-    color: "black",
-    textTransform: "unset !important",
+    color: 'black',
+    textTransform: 'unset !important',
   };
 
   const styleModalMenu = {
-    marginTop: 0.5,
+    //marginTop: 1,
     marginRight: 1,
     backgroundColor: '#E6F5D6',
     textTransform: 'unset !important',
     color: 'black',
   };
+
+  if (flagBegin) {
+    let ch = 1;
+    for (let i = 0; i < map.routes.length; i++) {
+      let nameZU = map.routes[i].description;
+      if (!nameZU) nameZU = 'без имени(' + ch++ + ')';
+      let maskName = {
+        name: nameZU,
+        delRec: false,
+      };
+      massName.push(maskName);
+    }
+    console.log('massName:', massName);
+    flagBegin = false;
+  }
 
   const [openSetMode, setOpenSetMode] = React.useState(true);
 
@@ -77,22 +95,20 @@ const SmdSelectMD = (props: { setOpen: any }) => {
 
   const StrokaTabl = () => {
     let resStr = [];
-    let ch = 1;
-    for (let i = 0; i < map.routes.length; i++) {
-      let nameZU = map.routes[i].description;
-      if (!nameZU) nameZU = "без имени(" + ch++ + ")";
+
+    for (let i = 0; i < massName.length; i++) {
       resStr.push(
         <Grid key={i} container>
-          <Grid item xs={9} sx={{ border: 1, textAlign: "center" }}>
+          <Grid item xs={9} sx={{ border: 1, textAlign: 'center' }}>
             <Button
               variant="contained"
               sx={styleBut01}
               //onClick={() => ClickKnop(i)}
             >
-              {nameZU}
+              {massName[i].name}
             </Button>
           </Grid>
-          <Grid item xs={3} sx={{ border: 1, textAlign: "center" }}>
+          <Grid item xs={3} sx={{ border: 1, textAlign: 'center' }}>
             <Button
               variant="contained"
               sx={styleBut02}
@@ -101,7 +117,7 @@ const SmdSelectMD = (props: { setOpen: any }) => {
               удалить
             </Button>
           </Grid>
-        </Grid>
+        </Grid>,
       );
     }
     return resStr;
@@ -114,12 +130,15 @@ const SmdSelectMD = (props: { setOpen: any }) => {
           <b>&#10006;</b>
         </Button>
 
-        <Typography variant="h6" sx={{ textAlign: "center" }}>
+        <Typography variant="h6" sx={{ textAlign: 'center' }}>
           Выбор ЗУ
         </Typography>
-        <Box sx={{ overflowX: "auto", height: "36vh" }}>{StrokaTabl()}</Box>
-        <Button sx={styleModalMenu}>Удалить отмеченные</Button>
-        <br />
+        <Box sx={{ border: 1, overflowX: 'auto', height: '69vh' }}>{StrokaTabl()}</Box>
+        <Box sx={{ marginTop: 1, textAlign: 'center' }}>
+          <Button sx={styleModalMenu}>Удалить отмеченные</Button>
+        </Box>
+
+        {/* <br /> */}
       </Box>
     </Modal>
   );
