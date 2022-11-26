cb248ed3a427dcd551fb6cbf8f23e39710a0bc0b
andrey-omsk-63 <andrey_omsk@mail.ru>
2022-11-26 18:45:49 +0600

    '26111845'

--- src/components/GsComponents/GsErrorMessage.tsx
+++ src/components/GsComponents/GsErrorMessage.tsx
@@ -1,14 +1,38 @@
-import * as React from "react";
+import * as React from 'react';
 
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
-import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
 
-import { styleModalEnd, styleSetInf } from '../MainMapStyle';
+//import { styleModalEnd } from '../MainMapStyle';
 
 const GsErrorMessage = (props: { sErr: string; setOpen: any }) => {
   const [openSet, setOpenSet] = React.useState(true);
+
+  const styleSetInf = {
+    position: 'absolute',
+    marginTop: '15vh',
+    marginLeft: '24vh',
+    width: 380,
+    bgcolor: 'background.paper',
+    border: '3px solid #000',
+    borderColor: 'red',
+    borderRadius: 2,
+    boxShadow: 24,
+    p: 1.5,
+  };
+
+  const styleModalEnd = {
+    position: 'absolute',
+    top: '0%',
+    left: 'auto',
+    right: '-0%',
+    height: '21px',
+    maxWidth: '2%',
+    minWidth: '2%',
+    color: 'red',
+  };
 
   const handleClose = () => {
     props.setOpen(false);
@@ -21,7 +45,7 @@ const GsErrorMessage = (props: { sErr: string; setOpen: any }) => {
         <Button sx={styleModalEnd} onClick={handleClose}>
           <b>&#10006;</b>
         </Button>
-        <Typography variant="h6" sx={{ textAlign: "center", color: "red" }}>
+        <Typography variant="h6" sx={{ textAlign: 'center', color: 'red' }}>
           {props.sErr}
         </Typography>
       </Box>
